0739ff65bb347ca93e57e13f8f1e80bd25bf904e
Tauhid <mdtauhid0171@gmail.com>
2021-07-04 07:19:38 +0600

    set product page slider
    set product page slider

--- blog/app/Http/Controllers/ProductController.php
+++ blog/app/Http/Controllers/ProductController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Product;
 class ProductController extends Controller
 {
-    //
+    
     function index(){
-        return "welcome";
+        $data = Product::all();
+       
+        return view('product',['products'=>$data]);
     }
 }
+// echo '<pre>';
+// print_r($data);
+// die();
