a2d037ad6681617b4c041d444776c49df2d758ae
Tauhid <mdtauhid0171@gmail.com>
2021-07-04 17:02:39 +0600

    Cart Product Listing
    Cart Product Listing

--- blog/app/Http/Controllers/ProductController.php
+++ blog/app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
@@ -9,35 +10,48 @@ use Session;
 
 class ProductController extends Controller
 {
-    
-    function index(){
+
+    function index()
+    {
         $data = Product::all();
-       
-        return view('product',['products'=>$data]);
+
+        return view('product', ['products' => $data]);
     }
 
-    function detail($id){
-       $data=  Product::find($id);
-       return view('detail',['product'=>$data]);
+    function detail($id)
+    {
+        $data =  Product::find($id);
+        return view('detail', ['product' => $data]);
     }
-    function  addToCart(Request $req){
-        if($req->session()->has('user')){
+    function  addToCart(Request $req)
+    {
+        if ($req->session()->has('user')) {
             $cart = new Cart();
-            $cart->user_id=$req->session()->get('user')['id'];
-            $cart->product_id=$req->product_id ;
+            $cart->user_id = $req->session()->get('user')['id'];
+            $cart->product_id = $req->product_id;
             $cart->save();
             return redirect('/');
-
-        }else{
+        } else {
             return redirect('/login');
         }
     }
 
-  static function cartItem(){
-          $userId=Session::get('user')['id'];
-          return Cart::where('user_id',$userId)->count();
+    static function cartItem()
+    {
+        $userId = Session::get('user')['id'];
+        return Cart::where('user_id', $userId)->count();
     }
 
+    function cartlist()
+    {
+        $userId = Session::get('user')['id'];
+        $products = DB::table('cart')
+            ->join('products', 'cart.product_id', '=', 'products.id')
+            ->where('cart.user_id', $userId)
+            ->get();
+
+        return view('cartlist', ['products' => $products]);
+    }
 }
 // echo '<pre>';
 // print_r($data);
